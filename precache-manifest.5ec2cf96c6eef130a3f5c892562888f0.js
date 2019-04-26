b76197247322ee4a773419a876e14afde9d25eeb
Hammed Oyedele <itz.harmid@gmail.com>
2019-04-26 09:37:21 +0100

    Updates

---
+++ precache-manifest.5ec2cf96c6eef130a3f5c892562888f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b62daebb0b796b719327",
+    "url": "/use-cookie/static/js/runtime~main.4df96015.js"
+  },
+  {
+    "revision": "f12f2bd7a802585a12c4",
+    "url": "/use-cookie/static/js/main.46fc1880.chunk.js"
+  },
+  {
+    "revision": "576fba37c835ae140c4b",
+    "url": "/use-cookie/static/js/2.49b330c2.chunk.js"
+  },
+  {
+    "revision": "f12f2bd7a802585a12c4",
+    "url": "/use-cookie/static/css/main.a63f2289.chunk.css"
+  },
+  {
+    "revision": "6d8f0a7237f652d7b4adea52fb7f2491",
+    "url": "/use-cookie/index.html"
+  }
+];
