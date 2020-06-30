6de1de455d675628705516cfcc46c33f3a5cc8c6
Hammed Oyedele <itz.harmid@gmail.com>
2020-06-30 17:38:09 +0100

    feat: add better typescript support

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,31 @@
 import { useState } from 'react'
 
+export type CookieOptions = Partial<{
+  expires: number
+  domain: string
+  path: string
+  secure: boolean
+  httpOnly: boolean
+  maxAge: number
+  sameSite: string
+}>
+
 export default function useCookie<T = any>(
   key: string,
   defaultValue?: any
-): [T, (value: any, options?: object) => void, () => void] {
+): [T, (value: T, options?: CookieOptions) => void, () => void] {
   const [value, setValue] = useState(() => {
     let match = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)')
     let value = match ? match[2] : defaultValue
+
     try {
       value = JSON.parse(value)
     } catch (_) { }
+
     return value as T
   })
 
-  const setCookie = (value: any, options?: object): void => {
+  const setCookie = (value: any, options?: CookieOptions): void => {
     let cookieOptions = {
       expires: 0,
       domain: '',
@@ -24,35 +36,46 @@ export default function useCookie<T = any>(
       sameSite: '',
       ...options
     }
+
     setValue(value)
+
     // if value is an array or a plain object, convert to JSON
     if (Array.isArray(value) || Object.prototype.toString.call(value) === '[object Object]') {
       value = JSON.stringify(value)
     }
+
     let cookie: string = `${key}=${value}`
+
     if (cookieOptions.expires) {
       let date: Date = new Date()
       date.setTime(date.getTime() + 1000 * cookieOptions.expires)
       cookie += `; Expires=${date.toUTCString()}`
     }
+
     if (cookieOptions.path) {
       cookie += `; Path=${cookieOptions.path}`
     }
+
     if (cookieOptions.domain) {
       cookie += `; Domain=${cookieOptions.domain}`
     }
+
     if (cookieOptions.maxAge) {
       cookie += `; Max-Age=${cookieOptions.maxAge}`
     }
+
     if (cookieOptions.sameSite) {
       cookie += `; SameSite=${cookieOptions.sameSite}`
     }
+
     if (cookieOptions.secure) {
       cookie += '; Secure'
     }
+
     if (cookieOptions.httpOnly) {
       cookie += '; HttpOnly'
     }
+
     document.cookie = cookie
   }
 
